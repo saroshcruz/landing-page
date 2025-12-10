8a7162b9e80503c32cd473ffeebb62d9afe2bc34
Sarosh <sarosh@Saroshs-MacBook-Air.local>
2025-12-10 13:20:10 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Typed.js effect
+var typed = new Typed('.typed', {
+  strings: ['Frontend Developer', 'JavaScript Enthusiast', 'Problem Solver'],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true
+});
+
+// Optional: add more scroll-triggered JS animations here
